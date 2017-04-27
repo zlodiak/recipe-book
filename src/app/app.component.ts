59b7704593802977d5b497117ddeb6a108d92e3b
kalinin <sergey.kalinin@eventplatform.ru>
2017-04-27 18:01:56 +0300

    nav markup ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'rb-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rb works!';
+  
 }
