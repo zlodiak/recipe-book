04fe584dddca781c6649e2fd533a2de2cf4133ea
kalinin <sergey.kalinin@eventplatform.ru>
2017-04-28 15:48:57 +0300

    recipe-add ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 @Component({
   selector: 'rb-root',
